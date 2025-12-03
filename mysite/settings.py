1771a126e1f738c7d26c5612c9922b592ab3e0c5
Roberto Ugalde <roberto.ugalde@conteyor.com>
2025-12-03 15:57:19 -0600

    fix Host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9@y1)$-2eby^6bassbeh(i6g4qure^(0swq@^e&qjck#ag@=gg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
